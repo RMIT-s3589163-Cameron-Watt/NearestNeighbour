058922a7f6a6fac3493a3476e523da332dfb05d7
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-04-05 23:30:41 +1000

    put print statement in

--- src/nearestNeigh/KDTreeNN.java
+++ src/nearestNeigh/KDTreeNN.java
@@ -44,7 +44,10 @@ public class KDTreeNN implements NearestNeigh{
     	for (Point point: points)
     		unsortedNodes.add(new Node(point, true, null, null, null));
     	this.root = splitAndAddToTree(unsortedNodes, this.root);
+    	//this can be removed, for debugging purposes only
     	List<Direction> list = new ArrayList<Direction>();
+    	printTree(root, list, 0);
+
     }
     
     /**
